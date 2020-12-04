09fbd3cc665bd030b742cfbdbe38d06c6beb913b
songkyoungsuk <rudtjrch@naver.com>
2020-12-04 19:44:59 +0900

    js arrow-up

--- main.js
+++ main.js
@@ -39,7 +39,20 @@ document.addEventListener('scroll', () =>{
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show arrow-up-Btn
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
 
+//Handle click on arrow up
+arrowUp.addEventListener('click', () =>{
+    scrollIntoView('#home');
+})
 
 
 
